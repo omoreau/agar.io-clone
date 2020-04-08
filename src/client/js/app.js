ee89c637aaad59831b12e76bde3a1ed4856241c3
omoreau <63359022+omoreau@users.noreply.github.com>
2020-04-08 16:23:44 -0400

    Update app.js

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -320,9 +320,9 @@ function drawFood(food) {
     graph.fillStyle = 'hsl(' + food.hue + ', 100%, 50%)';
     graph.lineWidth = foodConfig.border;
     graph.drawImage(jeremie,food.x,food.y);
-    drawCircle(food.x - player.x + global.screenWidth / 2,
+    /*drawCircle(food.x - player.x + global.screenWidth / 2,
                food.y - player.y + global.screenHeight / 2,
-               food.radius, global.foodSides);
+               food.radius, global.foodSides);*/
 }
 
 function drawVirus(virus) {
